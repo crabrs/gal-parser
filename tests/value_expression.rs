8505de3b73bd95784e0c8ce4cad59571a7a55317
antkiller996 <eadrenking@outlook.com>
2023-08-15 16:13:32 +0800

    format and fix numberic parsing

--- tests/value_expression.rs
+++ tests/value_expression.rs
@@ -15,16 +15,21 @@ fn result_expression() {
 
 #[test]
 fn case_expression() {
-    parse_assert!(Rule::case_expression, "CASE n WHEN IS NULL THEN 1 ELSE 0 END");
+    parse_assert!(
+        Rule::case_expression,
+        "CASE n WHEN IS NULL THEN 1 ELSE 0 END"
+    );
     parse_assert!(Rule::case_expression, "CASE WHEN a > b THEN 1 ELSE 0 END");
-    parse_assert!(Rule::case_expression, "CASE p WHEN IS NULL THEN -1 ELSE path_length(p) END");
+    parse_assert!(
+        Rule::case_expression,
+        "CASE p WHEN IS NULL THEN -1 ELSE path_length(p) END"
+    );
 }
 
 #[test]
 fn when_operand() {
     parse_assert!(Rule::when_operand, "IS NULL");
 }
-
 
 #[test]
 fn path_length_expression() {
@@ -33,22 +38,35 @@ fn path_length_expression() {
 
 #[test]
 fn common_value_expression() {
-    parse_assert!(Rule::common_value_expression, "DURATION_BETWEEN(n.creationDate, $creationDate)");
+    parse_assert!(
+        Rule::common_value_expression,
+        "DURATION_BETWEEN(n.creationDate, $creationDate)"
+    );
+    parse_assert!(Rule::common_value_expression, "-1");
 }
 
 #[test]
 fn duration_value_expression() {
-    parse_assert!(Rule::duration_value_expression, "DURATION_BETWEEN(n.creationDate, $creationDate)");
+    parse_assert!(
+        Rule::duration_value_expression,
+        "DURATION_BETWEEN(n.creationDate, $creationDate)"
+    );
 }
 
 #[test]
 fn boolean_value_expression() {
-    parse_assert!(Rule::boolean_value_expression, "DURATION_BETWEEN(n.creationDate, $creationDate) > 10");
+    parse_assert!(
+        Rule::boolean_value_expression,
+        "DURATION_BETWEEN(n.creationDate, $creationDate) > 10"
+    );
 }
 
 #[test]
 fn datetime_value_expression() {
-    parse_assert!(Rule::local_datetime_function, "local_datetime({epochMillis: foaf.birthday})");
+    parse_assert!(
+        Rule::local_datetime_function,
+        "local_datetime({epochMillis: foaf.birthday})"
+    );
 }
 
 #[test]
